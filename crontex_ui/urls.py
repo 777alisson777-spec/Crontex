af034c9b9a3f7c5adee6154f81557955d1f3d8fa
Alli Xshh <777alisson777@gmail.com>
2025-09-24 13:29:02 -0300

    Sync: atualiza telas e ajustes Django auth

--- crontex_ui/urls.py
+++ crontex_ui/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import DashboardView, AppLoginView, AppLogoutView
+from django.contrib.auth import views as auth_views
+from .views import DashboardView, AppLoginView, AppLogoutView, AppSignupView
 
 app_name = "crontex_ui"
 
@@ -7,4 +8,35 @@ urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("entrar/", AppLoginView.as_view(), name="login"),
     path("sair/", AppLogoutView.as_view(), name="logout"),
+    path("signup/", AppSignupView.as_view(), name="signup"),
+
+    # reset de senha
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
--- crontex_ui/views.py
+++ crontex_ui/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
+from .forms import SignupForm
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = "crontex_ui/dashboard.html"   # usa a subpasta namespaced
+    template_name = "crontex_ui/dashboard.html"
     login_url = "crontex_ui:login"
 
 class AppLoginView(LoginView):
@@ -13,3 +14,8 @@ class AppLoginView(LoginView):
 
 class AppLogoutView(LogoutView):
     next_page = reverse_lazy("crontex_ui:login")
+
+class AppSignupView(CreateView):
+    form_class = SignupForm
+    template_name = "crontex_ui/signup.html"
+    success_url = reverse_lazy("crontex_ui:login")
